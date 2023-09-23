cdcb5eba395f31776f48c947c5a1c4452efe460e
Rumaiq Arif <rumaiqahmad@gmail.com>
2023-09-23 15:48:17 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,11 @@
+let result = document.getElementById('result');
+function addToResult(value){
+    result.value += value;
+
+}
+function clearResult(){
+    result.value= '';
+}
+function calculate(){
+    result.value = eval(result.value);
+}
